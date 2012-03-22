3f26ba37988fab3191483afe75d6c92bc7a600b0
orazaro <oraz@rambler.ru>
2012-03-22 22:59:14 +0400

    hash table functions

---
+++ cs101/hash_table.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+def hashtable_get_bucket(htable,keyword):
+    return htable[hash_string(keyword, len(htable))]
+
+
+def hash_string(keyword,buckets):
+    out = 0
+    for s in keyword:
+        out = (out + ord(s)) % buckets
+    return out
+
+def make_hashtable(nbuckets):
+    table = []
+    for unused in range(0,nbuckets):
+        table.append([])
+    return table
+

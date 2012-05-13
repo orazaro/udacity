32beb571568de5db94c0a295df89d8cb5dd31373
orazaro <oraz@rambler.ru>
2012-05-13 21:57:09 +0400

    re_compiler

--- cs212/re_compiler.py
+++ cs212/re_compiler.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python
-#----------------
+# --------------
 # User Instructions
 #
-# Write the compiler for alt(x, y) in the same way that we 
-# wrote the compiler for lit(s) and seq(x, y). 
+# Fill out the function match(pattern, text), so that 
+# remainders is properly assigned. 
 
-'''
-def matchset(pattern, text):
-    op, x, y = components(pattern)
-    if 'lit' == op:
-        return set([text[len(x):]]) if text.startswith(x) else null
-    elif 'seq' == op:
-        return set(t2 for t1 in matchset(x, text) for t2 in matchset(y, t1))
-    elif 'alt' == op:
-        return matchset(x, text) | matchset(y, text)
-'''
+def match(pattern, text):
+    "Match pattern against start of text; return longest match found or None."
+    remainders = pattern(text) # your code here.
+    if remainders:
+        shortest = min(remainders, key=len)
+        return text[:len(text)-len(shortest)]
+    
+def lit(s): return lambda t: set([t[len(s):]]) if t.startswith(s) else null
+def seq(x, y): return lambda t: set().union(*map(y, x(t)))
+def alt(x, y): return lambda t: x(t) | y(t)
+def oneof(chars): return lambda t: set([t[1:]]) if (t and t[0] in chars) else null
+dot = lambda t: set([t[1:]]) if t else null
+eol = lambda t: set(['']) if t == '' else null
+def star(x): return lambda t: (set([t]) | 
+                               set(t2 for t1 in x(t) if t1 != t
+                                   for t2 in star(x)(t1)))
 
-def lit(s): return lambda text: set([text[len(s):]]) if text.startswith(s) else null
-
-def seq(x, y): return lambda text: set().union(*map(y, x(text)))
-
-def alt(x, y): return lambda text: x(text) | y(text)
-        
 null = frozenset([])
 
 def test():
-    g = alt(lit('a'), lit('b'))
-    f = alt(g,lit('ab'))
-    assert g('abc') == set(['bc'])
-    assert g('ax') == set(['x'])
-    assert f('abc') == set(['bc','c'])
-    return 'test passes'
+    assert match(star(lit('a')), 'aaaaabbbaa') == 'aaaaa'
+    assert match(lit('hello'), 'hello how are you?') == 'hello'
+    assert match(lit('x'), 'hello how are you?') == None
+    assert match(oneof('xyz'), 'x**2 + y**2 = r**2') == 'x'
+    assert match(oneof('xyz'), '   x is here!') == None
+    return 'tests pass'
 
 print test()
-
